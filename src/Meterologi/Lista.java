413071cb194d6932672de0424f22fea5ae611b09
Mikael <mikaelj_89@hotmail.com>
2011-04-12 15:56:34 +0200

    la til listefila alle skal extende

---
+++ src/Meterologi/Lista.java
@@ -0,0 +1,21 @@
+package Meterologi;
+
+import java.io.*;
+
+import Meterologi.Lister.*;
+
+public abstract class Lista {
+	private final String fila="listedata.dat";
+	DataListe dataliste = new DataListe();
+	ObjectInputStream input;
+	public Lista()
+	{
+		/*try{
+			input = new ObjectInputStream(
+								new BufferedInputStream(
+									new FileInputStream(fila)));
+		}catch(Exception e){System.out.println("kaos med lesing av fil");}
+		*/
+	}
+
+}
