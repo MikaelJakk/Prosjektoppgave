00d2058dfff79398558836a282dce80c255ea115
Mikael <mikaelj_89@hotmail.com>
2011-04-27 16:37:46 +0200

    naa funker det

--- src/Meterologi/RunProgram.java
+++ src/Meterologi/RunProgram.java
@@ -3,11 +3,9 @@ package Meterologi;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-
 import javax.swing.JFrame;
 
 public class RunProgram extends Lista{
-
 
 	public static HovedVindu vindu = null;
 
