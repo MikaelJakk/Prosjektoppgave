7bd78c8cdc3e28dd0cd40f85cb178dc50eb02d95
Nam Le <namlehuu@hotmail.com>
2011-04-27 16:32:31 +0200

    ordner seg

--- src/Meterologi/RunProgram.java
+++ src/Meterologi/RunProgram.java
@@ -1,24 +1,27 @@
 package Meterologi;
 
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
 
 public class RunProgram extends Lista{
 
-	public static HovedVindu vindu = null;
-
-	public static void main(String[] args)
-	{
-		vindu = new HovedVindu();
-		lesLista();
-
-		vindu.addWindowListener(new java.awt.event.WindowAdapter() {
-		    public void windowClosing(WindowEvent winEvt) {
-		        lagreLista();
-
+	
+	
+		  public static void main(String args[]) {
+		    JFrame frame = new JFrame("Window Listener");
+		    HovedVindu vindu = new HovedVindu();
+		    WindowListener listener = new WindowAdapter() {
+		      public void windowClosing(WindowEvent w) {
 		        System.exit(0);
-		    }
-		});
-		
-		
-	}
-}
+		      }
+		    };
+		    frame.addWindowListener(listener);
+		    frame.setSize(300, 300);
+		    frame.show();
+		  }
+		}
+	
+
