a4935d2246d8028079d6ecfa7439fd3510dc522d
Mikael <mikaelj_89@hotmail.com>
2011-04-27 16:34:41 +0200

    merget

--- src/Meterologi/RunProgram.java
+++ src/Meterologi/RunProgram.java
@@ -4,12 +4,10 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-<<<<<<< HEAD
 import javax.swing.JFrame;
 
 public class RunProgram extends Lista{
-=======
-public class RunProgram extends Lista{
+
 
 	public static HovedVindu vindu = null;
 
@@ -17,34 +15,5 @@ public class RunProgram extends Lista{
 	{
 		vindu = new HovedVindu();
 		lesLista();
-/*
-		vindu.addWindowListener(new java.awt.event.WindowAdapter() {
-		    public void windowClosing(WindowEvent winEvt) {
-		        lagreLista();
->>>>>>> 94f4ce3b28e794838d57256dc093bdb998bbca52
-
-	
-	
-		  public static void main(String args[]) {
-		    JFrame frame = new JFrame("Window Listener");
-		    HovedVindu vindu = new HovedVindu();
-		    WindowListener listener = new WindowAdapter() {
-		      public void windowClosing(WindowEvent w) {
-		        System.exit(0);
-<<<<<<< HEAD
-		      }
-		    };
-		    frame.addWindowListener(listener);
-		    frame.setSize(300, 300);
-		    frame.show();
-		  }
-		}
-	
-
-=======
-		    }
-		});
-		*/
 	}
 }
->>>>>>> 94f4ce3b28e794838d57256dc093bdb998bbca52
