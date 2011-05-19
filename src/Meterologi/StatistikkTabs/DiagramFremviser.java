aff95f15ec67b6c7328ed97b7b9bd2be690c93a7
Masta Nordengen <metall.thomas@gmail.com>
2011-05-19 04:23:42 +0200

    mergeFaen

---
+++ src/Meterologi/StatistikkTabs/DiagramFremviser.java
@@ -0,0 +1,24 @@
+package Meterologi.StatistikkTabs;
+
+
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+
+
+public class DiagramFremviser
+{
+	public static void main (String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+
+			public void run()
+			{
+				DiagramVindu diagram = new DiagramVindu();
+				diagram.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				diagram.pack();
+				diagram.setVisible(true);
+			}
+		});
+	}
+}
